03ef6968711f48ef45ab4055e706734b0af7f80f
Mehmet Teker <teker.mehmed@gmail.com>
2021-01-16 15:09:34 +0300

    Move ConnectionString connection to Startup.cs and editing directory for domain

---
+++ SehriWanBelgeseli.Api/Domain/DbContext/SehriWanBelgeselDBContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace SehriWanBelgeseli.Api.Domain
+{
+    public partial class SehriWanBelgeselDBContext : DbContext
+    {
+        public SehriWanBelgeselDBContext()
+        {
+        }
+
+        public SehriWanBelgeselDBContext(DbContextOptions<SehriWanBelgeselDBContext> options)
+            : base(options)
+        {
+        }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        }
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
--- SehriWanBelgeseli.Api/Startup.cs
+++ SehriWanBelgeseli.Api/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using SehriWanBelgeseli.Api.Domain;
 
 namespace SehriWanBelgeseli.Api
 {
@@ -25,6 +27,9 @@ namespace SehriWanBelgeseli.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<SehriWanBelgeselDBContext>(options => {
+                options.UseSqlServer(Configuration["ConnectionStrings:MyContext"]);
+            });
             services.AddControllers();
         }
 
