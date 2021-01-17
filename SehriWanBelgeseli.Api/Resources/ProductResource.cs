aef367f43259df997b84c3e9d4b2dff4bfbe2823
Mehmet Teker <teker.mehmed@gmail.com>
2021-01-17 08:33:53 +0300

    Creating Product resource class

---
+++ SehriWanBelgeseli.Api/Resources/ProductResource.cs
@@ -0,0 +1,11 @@
+﻿namespace SehriWanBelgeseli.Api.Resources
+{
+    public class ProductResource
+    {
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int UnitInStock { get; set; }
+    }
+}
