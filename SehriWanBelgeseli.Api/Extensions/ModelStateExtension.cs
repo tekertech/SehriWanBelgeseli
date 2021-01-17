b8cc08c6d4cea99aea115eda45eeae7d1a2a46ee
Mehmet Teker <teker.mehmed@gmail.com>
2021-01-17 16:58:41 +0300

    Model State Extension class added.

---
+++ SehriWanBelgeseli.Api/Extensions/ModelStateExtension.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SehriWanBelgeseli.Api.Extensions
+{
+    public static class ModelStateExtension
+    {
+        public static List<string> GetErrorMessage(this ModelStateDictionary dictionary)  
+        {
+            return dictionary.SelectMany(m => m.Value.Errors).Select(x => x.ErrorMessage).ToList();
+        }
+    }
+}
