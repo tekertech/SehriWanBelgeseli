afef4f667616f8d1df23cf54f6677e2d0679478d
Mehmet Teker <teker.mehmed@gmail.com>
2021-01-17 08:49:17 +0300

    Adding AutoMapper library and creating Product mapping class.

---
+++ SehriWanBelgeseli.Api/Mapping/ProductMapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using SehriWanBelgeseli.Api.Domain;
+using SehriWanBelgeseli.Api.Resources;
+
+namespace SehriWanBelgeseli.Api.Mapping
+{
+    public class ProductMapping:Profile
+    {
+        public ProductMapping()
+        {
+            CreateMap<ProductResource, Product>();
+            CreateMap<Product, ProductResource>();
+        }
+    }
+}
