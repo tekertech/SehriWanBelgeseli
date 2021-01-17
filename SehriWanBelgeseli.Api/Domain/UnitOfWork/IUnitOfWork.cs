70fa7f8f44d026fe938744fcfc9f357a8880235d
Mehmet Teker <teker.mehmed@gmail.com>
2021-01-17 07:47:11 +0300

    The UnitOfWork design pattern is applied to the project.

---
+++ SehriWanBelgeseli.Api/Domain/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SehriWanBelgeseli.Api.Domain.UnitOfWork
+{
+    interface IUnitOfWork {
+        Task CompleteAsync();
+    }
+}
---
+++ SehriWanBelgeseli.Api/Domain/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SehriWanBelgeseli.Api.Domain.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly SehriWanBelgeselDBContext dBContext;
+
+        public UnitOfWork(SehriWanBelgeselDBContext dBContext)
+        {
+            this.dBContext = dBContext;
+        }
+        public async Task CompleteAsync()
+        {
+            await dBContext.SaveChangesAsync();
+        }
+    }
+}
